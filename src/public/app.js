021408b804dc9d0eaea3ccd27701a487634b0aaf
volad <artosan360@gmail.com>
2022-09-15 17:11:25 +0300

    fixed body overflow, paddings of sections for mobile

--- src/public/app.js
+++ src/public/app.js
@@ -1,9 +1,11 @@
-const turnToggle = (element) => element.classList.toggle("open");
+const turnToggle = (element, togleName) => element.classList.toggle(togleName);
 const burgerMenu = document.querySelector(".burger");
 const sides = document.querySelectorAll(".side");
 const header = document.querySelector("header");
+const body = document.querySelector("body");
 burgerMenu.addEventListener("click", () => {
   [burgerMenu, sides[0], sides[1], header].forEach((value) => {
-    turnToggle(value);
+    turnToggle(value, "open");
   });
+  turnToggle(body, "overflow");
 });
